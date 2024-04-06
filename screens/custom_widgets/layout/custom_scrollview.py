bef855ef055ff8a372e05f7e03d7371ebf196083
LupaDevStudio <lupa.dev.studio@gmail.com>
2024-04-06 21:54:58 +0200

    improve scrollview reaction

--- screens/custom_widgets/layout/custom_scrollview.py
+++ screens/custom_widgets/layout/custom_scrollview.py
@@ -14,4 +14,16 @@ from kivy.uix.scrollview import ScrollView
 
 
 class CustomScrollview(ScrollView):
+    # def on_touch_down(self, touch):
+    #     print(touch.x)
+    #     return super().on_touch_down(touch)
+
+    # def on_scroll_start(self, touch, check_children=True):
+    #     print("scroll start")
+    #     return super().on_scroll_start(touch, check_children)
+
+    # def on_scroll_move(self, touch):
+    #     print("scroll move")
+    #     return super().on_scroll_move(touch)
+
     pass
