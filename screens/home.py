a4a4fbb22f97f82350db7ca873abe6c516f6d026
LupaDevStudio <lupa.dev.studio@gmail.com>
2024-05-31 09:08:50 +0200

    update version number to 1.0.0

--- screens/home.py
+++ screens/home.py
@@ -47,7 +47,7 @@ class HomeScreen(LinconymScreen):
         current_music = USER_DATA.settings["current_music"]
         if music_mixer.musics[current_music].state == "stop":
             music_mixer.play(current_music, loop=True)
-        
+
         today_date = datetime.today().strftime('%m/%d/%Y')
         if USER_DATA.ads["current_day_date"] != today_date:
             USER_DATA.ads["current_day_date"] = today_date
@@ -56,12 +56,12 @@ class HomeScreen(LinconymScreen):
 
         if not USER_DATA.ads["has_seen_daily_wheel"]:
             USER_DATA.ads["has_seen_daily_wheel"] = True
-            popup = DailyWheelPopup(
-                font_ratio=self.font_ratio,
-                primary_color=self.primary_color,
-                secondary_color=self.secondary_color
-            )
-            popup.open()
+            # popup = DailyWheelPopup(
+            #     font_ratio=self.font_ratio,
+            #     primary_color=self.primary_color,
+            #     secondary_color=self.secondary_color
+            # )
+            # popup.open()
         USER_DATA.save_changes()
         return super().on_enter(*args)
 
